b8c2a775ad6271d0cc0cf3293c72cfbc47e2aad0
CathyChen-Cat <qoobcat@hotmail.com>
2021-09-05 17:55:47 +1200

    Front-end content. Layout using Bootstrap.

---
+++ OnlineCarSale/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineCarSale.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult SellerDetail()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult AddCar()
+        {
+            return View();
+        }
+    }
+}
---
+++ OnlineCarSale/Controllers/BuyCarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineCarSale.Controllers
+{
+    public class BuyCarController : Controller
+    {
+        // GET: BuyCar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
