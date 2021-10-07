cf6e913738fdb5f24c31ed922e1a0f9f936ddd5e
CathyChen-Cat <qoobcat@hotmail.com>
2021-10-07 13:36:23 +1300

    Added database, create function for feedback form.

--- OnlineCarSale/Controllers/FeedbackController.cs
+++ OnlineCarSale/Controllers/FeedbackController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using OnlineCarSale.Models;
 
 namespace OnlineCarSale.Controllers
 {
@@ -12,6 +13,25 @@ namespace OnlineCarSale.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Create([Bind(Include = "Name, Email, InfoRadio, NeedRadio, Comment")] Feedback feedback)
+        {
+            OnlineCarSaleEntities db = new OnlineCarSaleEntities();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Feedbacks.Add(feedback);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Unable to save changes.");
+            }
+            return View(feedback);
         }
     }
 }
